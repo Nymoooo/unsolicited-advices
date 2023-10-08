edd4447cd7a568769368d147eeeb8384c105a4db
[Ezekiel Narvasa ] <114404058+Nymoooo@users.noreply.github.com>
2023-10-08 20:53:44 +0800

    .

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+export default function App() {
+  const [advice, setAdvice] = useState('');
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    try {
+      const res = await fetch('https://api.adviceslip.com/advice');
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await res.json();
+      setAdvice(data.slip.advice);
+      setCount((c) => c + 1);
+    } catch (error) {
+      console.error('Error fetching advice:', error);
+    }
+  }
+
+  useEffect(() => {
+    // Do not fetch advice initially when the component mounts
+    // This will ensure the advice is fetched only after the button is clicked.
+  }, []);
+
+  return (
+    <div>
+      <h1>Click the button below for unsolicited advices. </h1>
+      {advice !== '' && (
+        <>
+          <h2>{advice}</h2>
+          <Message count={count} />
+        </>
+      )}
+      <button onClick={getAdvice}>Click Me!</button>
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces of advices.
+    </p>
+  );
+}
